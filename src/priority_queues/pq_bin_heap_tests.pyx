7703138a1ab3c60e2f4cc1cf18488079bd735f95
djfrancesco <pacullfrancois@gmail.com>
2022-08-24 17:29:59 +0200

    improved a test

--- src/priority_queues/pq_bin_heap_tests.pyx
+++ src/priority_queues/pq_bin_heap_tests.pyx
@@ -30,45 +30,54 @@ cpdef insert_01():
 
     init_heap(&bheap, 4)
 
-
+    elem_idx = 1
     key = 3.0
-    min_heap_insert(&bheap, 1, key)
-    
+    min_heap_insert(&bheap, elem_idx, key)
     A_ref = [1, 4, 4, 4]
     for i in range(4):
         assert bheap.A[i] == A_ref[i]
-    assert bheap.elements[1].key == key
-    assert bheap.elements[1].state == IN_HEAP
+    assert bheap.elements[elem_idx].key == key
+    assert bheap.elements[elem_idx].state == IN_HEAP
+    assert bheap.elements[1].node_idx == 0
     assert bheap.size == 1
 
+    elem_idx = 0
     key = 2.0
-    min_heap_insert(&bheap, 0, key)
-
+    min_heap_insert(&bheap, elem_idx, key)
     A_ref = [0, 1, 4, 4]
     for i in range(4):
         assert bheap.A[i] == A_ref[i]
-    assert bheap.elements[0].key == key
-    assert bheap.elements[0].state == IN_HEAP
+    assert bheap.elements[elem_idx].key == key
+    assert bheap.elements[elem_idx].state == IN_HEAP
+    assert bheap.elements[0].node_idx == 0
+    assert bheap.elements[1].node_idx == 1
     assert bheap.size == 2
 
+    elem_idx = 3
     key = 4.0
-    min_heap_insert(&bheap, 3, key)
-
+    min_heap_insert(&bheap, elem_idx, key)
     A_ref = [0, 1, 3, 4]
     for i in range(4):
         assert bheap.A[i] == A_ref[i]
-    assert bheap.elements[3].key == key
-    assert bheap.elements[3].state == IN_HEAP
+    assert bheap.elements[elem_idx].key == key
+    assert bheap.elements[elem_idx].state == IN_HEAP
+    assert bheap.elements[0].node_idx == 0
+    assert bheap.elements[1].node_idx == 1
+    assert bheap.elements[3].node_idx == 2
     assert bheap.size == 3
 
+    elem_idx = 2
     key = 1.0
-    min_heap_insert(&bheap, 2, key)
-
+    min_heap_insert(&bheap, elem_idx, key)
     A_ref = [2, 0, 3, 1]
     for i in range(4):
         assert bheap.A[i] == A_ref[i]
     assert bheap.elements[2].key == key
     assert bheap.elements[2].state == IN_HEAP
+    assert bheap.elements[0].node_idx == 1
+    assert bheap.elements[1].node_idx == 3
+    assert bheap.elements[2].node_idx == 0
+    assert bheap.elements[3].node_idx == 2
     assert bheap.size == 4
 
     free_heap(&bheap)
@@ -81,10 +90,15 @@ cpdef peek_01():
 
     init_heap(&bheap, 6)
     min_heap_insert(&bheap, 0, 9.0)
+    assert peek(&bheap) == 9.0
     min_heap_insert(&bheap, 1, 9.0)
+    assert peek(&bheap) == 9.0
     min_heap_insert(&bheap, 2, 9.0)
+    assert peek(&bheap) == 9.0
     min_heap_insert(&bheap, 3, 5.0)
+    assert peek(&bheap) == 5.0
     min_heap_insert(&bheap, 4, 3.0)
+    assert peek(&bheap) == 3.0
     min_heap_insert(&bheap, 5, 1.0)
-    assert peek(&bheap) == 1
+    assert peek(&bheap) == 1.0
     free_heap(&bheap)
