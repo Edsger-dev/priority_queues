c51cc3e5c7a3db52bd4e064357553eb68327ea09
djfrancesco <pacullfrancois@gmail.com>
2022-08-25 21:16:37 +0200

    style

--- tests/test_pq_bin_heaps.py
+++ tests/test_pq_bin_heaps.py
@@ -4,26 +4,34 @@ from priority_queues.pq_bin_heap_tests import *
 def test_init_01():
     init_01()
 
+
 def test_insert_01():
     insert_01()
+
 
 def test_insert_02():
     insert_02()
 
+
 def test_insert_03():
     insert_03()
+
 
 def test_peek_01():
     peek_01()
 
+
 def test_extract_min_01():
     extract_min_01()
+
 
 def test_is_empty_01():
     is_empty_01()
 
+
 def test_decrease_key_from_element_index_01():
     decrease_key_from_element_index_01()
+
 
 def test_sort_01():
     sort_01(1000)
