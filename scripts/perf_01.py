b764736af5f6a5a2a9780c610f065e3891582ec7
djfrancesco <pacullfrancois@gmail.com>
2022-08-30 10:15:57 +0200

    new script

--- scripts/perf_01.py
+++ scripts/perf_01.py
@@ -6,12 +6,19 @@ from priority_queues.shortest_path import ShortestPath
 from scipy.sparse import coo_array, csr_matrix
 from scipy.sparse.csgraph import dijkstra
 
+# NETWORK_FILE_PATH = "/home/francois/Workspace/posts_priority_queue/data/NY/NY.parquet"
 NETWORK_FILE_PATH = "/home/francois/Workspace/posts_priority_queue/data/USA/USA.parquet"
+IDX_FROM = 1000
 
 edges_df = pd.read_parquet(NETWORK_FILE_PATH)
 edges_df.rename(
     columns={"id_from": "source", "id_to": "target", "tt": "weight"}, inplace=True
 )
+
+print(edges_df.head(3))
+# print(edges_df.min())
+# print(edges_df.max())
+# print(edges_df.isna().any())
 
 # SciPy
 
@@ -31,7 +38,7 @@ print(f"SciPy Prepare the data - Elapsed time: {elapsed_time:6.2f} s")
 start = perf_counter()
 
 dist_matrix_ref = dijkstra(
-    csgraph=graph_csr, directed=True, indices=0, return_predecessors=False
+    csgraph=graph_csr, directed=True, indices=IDX_FROM, return_predecessors=False
 )
 
 end = perf_counter()
@@ -41,23 +48,21 @@ print(f"SciPy Dijkstra - Elapsed time: {elapsed_time:6.2f} s")
 # In-house
 # without graph permutation
 # return_inf=True
-
 start = perf_counter()
-
 sp = ShortestPath(edges_df, orientation="one-to-all", check_edges=False, permute=False)
-
 end = perf_counter()
 elapsed_time = end - start
 print(f"PQ Prepare the data - Elapsed time: {elapsed_time:6.2f} s")
 
 start = perf_counter()
-
-path_lengths = sp.run(vertex_idx=0, return_inf=True)
+path_lengths = sp.run(vertex_idx=IDX_FROM, return_inf=True)
 dist_matrix = path_lengths.values
-
 end = perf_counter()
 elapsed_time = end - start
 print(f"PQ Dijkstra - Elapsed time: {elapsed_time:6.2f} s")
 
-# not really understanding what happens when computing the difference of inf
-assert np.testing.assert_array_almost_equal(dist_matrix, dist_matrix_ref, decimal=2)
+time_df = sp.get_timings()
+print(time_df)
+
+print(f"{len(edges_df)} edges and {vertex_count} vertices")
+assert np.allclose(dist_matrix, dist_matrix_ref, rtol=1e-05, atol=1e-08, equal_nan=True)
