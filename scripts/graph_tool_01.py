28390d7ceb823d29aec45b357e56c03d45850953
djfrancesco <pacullfrancois@gmail.com>
2022-09-06 11:32:03 +0200

    graph-tool shortest distance

--- scripts/graph_tool_01.py
+++ scripts/graph_tool_01.py
@@ -1,7 +1,13 @@
-
 from argparse import ArgumentParser
+from time import perf_counter
 
+import graph_tool as gt
+from graph_tool import topology
+import numpy as np
 import pandas as pd
+
+from priority_queues.shortest_path import ShortestPath
+
 
 parser = ArgumentParser(description="Command line interface to perf_01.py")
 parser.add_argument(
@@ -41,3 +47,64 @@ edges_df.rename(
 )
 
 print(edges_df.head(3))
+
+# Graph-tools
+# ===========
+
+start = perf_counter()
+
+# create the graph
+g = gt.Graph(directed=True)
+
+# create the vertices
+vertex_count = edges_df[["source", "target"]].max().max() + 1
+g.add_vertex(vertex_count)
+
+# create the edges
+g.add_edge_list(edges_df[["source", "target"]].values)
+
+# edge property for the travel time
+eprop_t = g.new_edge_property("float")
+g.edge_properties["t"] = eprop_t  # internal property
+g.edge_properties["t"].a = edges_df["weight"].values
+
+end = perf_counter()
+elapsed_time = end - start
+print(f"GT Load the graph - Elapsed time: {elapsed_time:6.2f} s")
+
+
+start = perf_counter()
+dist = topology.shortest_distance(
+    g, source=g.vertex(IDX_FROM), weights=g.ep.t, negative_weights=False, directed=True
+)
+dist_matrix_ref = dist.a
+end = perf_counter()
+elapsed_time = end - start
+print(f"GT Dijkstra - Elapsed time: {elapsed_time:6.2f} s")
+
+# In-house
+# ========
+
+# without graph permutation
+# return_inf=True
+start = perf_counter()
+sp = ShortestPath(edges_df, orientation="one-to-all", check_edges=False, permute=False)
+end = perf_counter()
+elapsed_time = end - start
+print(f"PQ Prepare the data - Elapsed time: {elapsed_time:6.2f} s")
+
+start = perf_counter()
+dist_matrix = sp.run(vertex_idx=IDX_FROM, return_inf=True, return_Series=False)
+end = perf_counter()
+elapsed_time = end - start
+print(f"PQ Dijkstra - Elapsed time: {elapsed_time:6.2f} s")
+
+time_df = sp.get_timings()
+print(time_df)
+
+
+print(f"{len(edges_df)} edges and {vertex_count} vertices")
+assert np.allclose(dist_matrix, dist_matrix_ref, rtol=1e-05, atol=1e-08, equal_nan=True)
+
+
+print("done")
