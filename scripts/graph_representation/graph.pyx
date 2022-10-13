f6dbe69aaff7d2878cf6f8d3e41fd992fc74ba49
djfrancesco <pacullfrancois@gmail.com>
2022-10-13 14:57:28 +0200

    wip

--- scripts/graph_representation/graph.pyx
+++ scripts/graph_representation/graph.pyx
@@ -3,7 +3,9 @@
 from time import perf_counter
 
 cimport numpy as cnp
+from cython.parallel import prange
 from libc.stdlib cimport free, malloc
+import psutil
 
 
 cpdef void loop_CSR(
@@ -16,7 +18,7 @@ cpdef void loop_CSR(
         ssize_t tail_vert_idx, head_vert_idx, ptr
         cnp.float64_t edge_weight
 
-    for tail_vert_idx in range(vertex_count):
+    for tail_vert_idx in range(<size_t>vertex_count):
     # for tail_vert_idx in range(10):
         # print(f"- {tail_vert_idx}")
         for ptr in range(csr_indptr[tail_vert_idx], csr_indptr[tail_vert_idx + 1]):
@@ -30,12 +32,58 @@ cdef struct ForwardStar:
     cnp.float64_t* weights
 
 cdef struct AdjacencyVectors:
+    size_t vertex_count
     ForwardStar* forward_stars
 
-cdef void init_adjacency_vectors(AdjacencyVectors* adj_vec, int vertex_count):
+cdef void init_adjacency_vectors(AdjacencyVectors* adjvec, int vertex_count):
 
-    adj_vec.forward_stars = <ForwardStar*> malloc(vertex_count * sizeof(ForwardStar))
+    adjvec.vertex_count = <size_t> vertex_count
+    adjvec.forward_stars = <ForwardStar*> malloc(vertex_count * sizeof(ForwardStar))
 
+cdef void create_FSV(
+    AdjacencyVectors* adjvec,
+    ssize_t[::1] csr_indptr,
+    ssize_t[::1] csr_indices,
+    cnp.float64_t[::1] csr_data,
+    int n_jobs) nogil:
+    
+    cdef:
+        size_t i, tail_vert_idx, size, ptr
+
+    for tail_vert_idx in prange(
+        adjvec.vertex_count,
+        num_threads=n_jobs):
+        size = csr_indptr[tail_vert_idx + 1] - csr_indptr[tail_vert_idx]
+        adjvec.forward_stars[tail_vert_idx].size = size
+        adjvec.forward_stars[tail_vert_idx].vertices = <ssize_t*> malloc(size * sizeof(ssize_t))
+        adjvec.forward_stars[tail_vert_idx].weights = <cnp.float64_t*> malloc(size * sizeof(cnp.float64_t))
+        for i in range(size):
+            ptr = csr_indptr[tail_vert_idx] + i
+            adjvec.forward_stars[tail_vert_idx].vertices[i] = csr_indices[ptr]
+            adjvec.forward_stars[tail_vert_idx].weights[i] = csr_data[ptr]
+
+cdef void loop_FSV_inner(AdjacencyVectors* adjvec) nogil:
+    
+    cdef:
+        size_t i, tail_vert_idx, head_vert_idx
+        cnp.float64_t edge_weight
+
+    for tail_vert_idx in range(adjvec.vertex_count):
+        for i in range(adjvec.forward_stars[tail_vert_idx].size):
+            head_vert_idx = adjvec.forward_stars[tail_vert_idx].vertices[i]
+            edge_weight = adjvec.forward_stars[tail_vert_idx].weights[i] 
+
+cdef void free_adjacency_vectors(AdjacencyVectors* adjvec, int n_jobs) nogil:
+
+    cdef ssize_t tail_vert_idx
+
+    for tail_vert_idx in prange(
+        adjvec.vertex_count,
+        num_threads=n_jobs):
+        free(adjvec.forward_stars[tail_vert_idx].vertices)
+        free(adjvec.forward_stars[tail_vert_idx].weights)
+
+    free(adjvec.forward_stars)
 
 cpdef void loop_FSV(
     ssize_t[::1] csr_indptr,
@@ -44,37 +92,39 @@ cpdef void loop_FSV(
     int vertex_count):
 
     cdef:
-        AdjacencyVectors adj_vec
-        size_t size, tail_vert_idx, head_vert_idx, i
-        cnp.float64_t edge_weight
+        AdjacencyVectors adjvec
+        int n_jobs
 
-    init_adjacency_vectors(&adj_vec, vertex_count)
-    for tail_vert_idx in range(vertex_count):
-        size = csr_indptr[tail_vert_idx + 1] - csr_indptr[tail_vert_idx]
-        adj_vec.forward_stars[tail_vert_idx].size = size
-        adj_vec.forward_stars[tail_vert_idx].vertices = <ssize_t*> malloc(size * sizeof(ssize_t))
-        adj_vec.forward_stars[tail_vert_idx].weights = <cnp.float64_t*> malloc(size * sizeof(cnp.float64_t))
-        for i in range(size):
-            ptr = csr_indptr[tail_vert_idx] + i
-            adj_vec.forward_stars[tail_vert_idx].vertices[i] = csr_indices[ptr]
-            adj_vec.forward_stars[tail_vert_idx].weights[i] = csr_data[ptr]
+    n_jobs = psutil.cpu_count()
 
     start = perf_counter()
 
-    for tail_vert_idx in range(vertex_count):
-    # for tail_vert_idx in range(10):
-        # print(f"- {tail_vert_idx}")
-        for i in range(adj_vec.forward_stars[tail_vert_idx].size):
-            head_vert_idx = adj_vec.forward_stars[tail_vert_idx].vertices[i]
-            edge_weight = adj_vec.forward_stars[tail_vert_idx].weights[i]  
-            # print(f"{head_vert_idx} : {edge_weight}")          
+    init_adjacency_vectors(&adjvec, vertex_count)
+
+    end = perf_counter()
+    elapsed_time = end - start
+    print(f"FSV init - Elapsed time: {elapsed_time:6.2f} s")
+
+    start = perf_counter()
+
+    create_FSV(&adjvec, csr_indptr, csr_indices, csr_data, n_jobs)
+
+    end = perf_counter()
+    elapsed_time = end - start
+    print(f"FSV create - Elapsed time: {elapsed_time:6.2f} s")
+
+    start = perf_counter()
+
+    loop_FSV_inner(&adjvec)
 
     end = perf_counter()
     elapsed_time = end - start
     print(f"FSV loop - Elapsed time: {elapsed_time:6.2f} s")
 
-    for tail_vert_idx in range(vertex_count):
-        free(adj_vec.forward_stars[tail_vert_idx].vertices)
-        free(adj_vec.forward_stars[tail_vert_idx].weights)
+    start = perf_counter()
 
-    free(adj_vec.forward_stars)
+    free_adjacency_vectors(&adjvec, n_jobs)
+
+    end = perf_counter()
+    elapsed_time = end - start
+    print(f"FSV free - Elapsed time: {elapsed_time:6.2f} s")
